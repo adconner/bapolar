b68820a09841a0fbb17eb23d8d29a927c311169b
Austin Conner <aconner.vu@gmail.com>
2024-04-11 21:13:57 -0400

    remove stack size limit

--- all.py
+++ all.py
@@ -3,6 +3,11 @@ from collections import Counter
 
 from sage.libs.singular.function_factory import ff
 
+import resource
+import sys
+resource.setrlimit(resource.RLIMIT_STACK, [0x10000000, resource.RLIM_INFINITY])
+sys.setrecursionlimit(0x100000)
+
 attach('bapolar.py')
 attach('misc.py')
 attach('symmetry.py')
