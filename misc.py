c4ad8851dfaa51b1dd35fb8b613f94154476a987
Austin Conner <aconner.vu@gmail.com>
2024-05-03 18:48:23 -0400

    cosmetic changes

--- misc.py
+++ misc.py
@@ -218,7 +218,7 @@ class BinaryProgramSCIPReopt:
         self.M.freeReoptSolve()
         self.M.chgReoptObjective(quicksum( getVar(x) if v == 1 else -getVar(x) for x,v in self.psol),"maximize")
         target = len([_ for _,v in self.psol if v==1])
-        # self.M.hideOutput(False)
+        self.M.hideOutput(False)
         self.M.optimize()
         if self.M.getStatus() != 'optimal' or int(self.M.getObjVal()) < target:
             rem = list(self.infeas.iter_sets(Slo=self.psol))
