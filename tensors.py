d34df42e855692374853b84e89750aa771ff0550
Austin Conner <aconner.vu@gmail.com>
2024-07-10 19:02:44 -0400

    matrixmult can now give iterated

--- tensors.py
+++ tensors.py
@@ -2,15 +2,15 @@ from sage.all import PolynomialRing
 from itertools import product
 
 def matrixmult(*idims):
-    dims = tuple(a*b for a,b in zip(idims,idims[1:]+(idims[0],)))
-    R = PolynomialRing(QQ,['%s%d%d' % (c,i,j) for c,a,b in zip('uvwxyzabcdefghijklmnopqrst', idims,idims[1:]+(idims[0],)) 
+    dims = tuple(a*b for a,b in zip(idims,idims[1:]+idims[:1]))
+    R = PolynomialRing(QQ,['%s%d%d' % (c,i,j) for c,a,b in zip('uvwxyzabcdefghijklmnopqrst', idims,idims[1:]+idims[:1]) 
                            for i,j in product(range(a),range(b))])
     xs = []
     c = 0
-    for a,b in zip(idims,idims[1:]+(idims[0],)):
+    for a,b in zip(idims,idims[1:]+idims[:1]):
         xs.append(R.gens()[c:c+a*b])
         c += a*b
-    T = sum(prod(u[i*a+j] for u,i,j,a in zip(xs,ixs,ixs[1:]+(ixs[0],),idims[1:]+(idims[0],)))
+    T = sum(prod(u[i*a+j] for u,i,j,a in zip(xs,ixs,ixs[1:]+(ixs[0],),idims[1:]+idims[:1]))
             for ixs in product(*map(range,idims)))
     return (T,dims)
 
